014641082d20884e5e4ef5a91ac29235a1ca1dbc
JKetelaar <jeroen@ketelaar.me>
2017-05-09 23:03:01 +0200

    [FEATURE] Added base for commands, including `parabot start`

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 #!/usr/bin/env node
-console.log('Welcome to the Parabot CLI');
+console.log('Welcome to the Parabot CLI, note that this is still in Alpha');
+var program = require('commander');
+var chalk = require('chalk');
+var http = require('http');
+var fs = require('fs');
+var download = require('download-file');
+
+program
+    .arguments('<command>')
+    .option('-u, --username <username>', 'The user to authenticate as')
+    .option('-p, --password <password>', 'The user\'s password')
+    .action(function (command) {
+        switch (command) {
+            case "start":
+                var file = fs.createWriteStream("Parabot.jar");
+                var request = http.get("http://v3.bdn.parabot.org/api/bot/download/client", function (response) {
+                    response.pipe(file);
+                });
+                break;
+            case "init":
+
+                break;
+            default:
+                console.error(chalk.red(command + " is an invalid command"));
+                break;
+        }
+    })
+    .parse(process.argv);

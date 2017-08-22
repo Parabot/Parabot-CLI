620c1e2afec15fe987726377c7b3c73e29328398
Emma Stone <capslock_donater@hotmail.com>
2017-08-22 18:36:20 +0100

    [FEATURE] New arguments

--- index.js
+++ index.js
@@ -4,20 +4,44 @@ var program = require('commander');
 var chalk = require('chalk');
 var http = require('http');
 var fs = require('fs');
-var download = require('download-file');
 
 program
     .arguments('<command>')
     .option('-u, --username <username>', 'The user to authenticate as')
     .option('-p, --password <password>', 'The user\'s password')
+    .option('-n, --nightly', 'Is client nightly?')
+    .option('-nv, --noverify', 'Java command noverify')
+    .option('-l, --local', 'Load local scripts/servers')
+    .option('-v, --verbose', 'Verbose mode')
+    .option('-d, --dump', 'Dump data')
     .action(function (command) {
         switch (command) {
             case "start":
                 console.log("Downloading Parabot");
 
                 var file = fs.createWriteStream("Parabot.jar");
-                var request = http.get("http://v3.bdn.parabot.org/api/bot/download/client", function (response) {
+                var request = http.get("http://v3.bdn.parabot.org/api/bot/download/client?nightly=" + program.nightly, function (response) {
                     response.pipe(file);
+                    file.on('finish', function () {
+                        var username = program.username;
+                        var password = program.password;
+                        var local = program.local ? "-loadlocal" : "";
+                        var verbose = program.verbose ? "-v" : "";
+                        var dump = program.dump ? "-dump" : "";
+                        var clearcache = program.clearcache ? "-clearcache" : "";
+                        var noverify = program.noverify ? "-noverify" : "";
+
+                        var exec = require('child_process').exec;
+                        module.exports = exec("java -jar " + noverify + " ./Parabot.jar -login "
+                            + username + " " + password + " "
+                            + local + " " + verbose + " " + clearcache + " " + dump,
+                            function (error, stdout) {
+                                console.log('Output -> ' + stdout);
+                                if (error !== null) {
+                                    console.log("Error -> " + error);
+                                }
+                            });
+                    });
 
                     console.log("Downloaded Parabot");
                 });
